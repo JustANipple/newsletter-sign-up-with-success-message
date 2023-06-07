56fffd159c1cd95273b9c58371f47bf921b3e248
JustANipple <prandisamuel99@gmail.com>
2023-06-07 16:32:19 +0200

    modified:   index.html 	modified:   scripts/script.js 	modified:   styles/main.css

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,34 @@
+const emailInput = document.querySelector("#form_email");
+const labelError = document.querySelector("#form_label span");
+const subscribeSection = document.querySelector("#subscribe");
+const successSection = document.querySelector("#success");
+
 document.querySelector("#form_btn").addEventListener("click", (e) => {
-    const emailInput = document.querySelector("#form_email");
+    e.preventDefault();
     if(emailInput.value.trim() === "") {
         emailInput.required = true;
-        e.preventDefault();
+        labelError.style.visibility = "visible";
+        labelError.style.opacity = "1";
     }
+    if(emailInput.checkValidity()) {
+        document.querySelector("#success_email_sent").textContent = emailInput.value;
+        subscribeSection.style.animation = "cardRotationHide 1s ease-in-out forwards";
+        successSection.style.animation = "cardRotationShow 1s ease-in-out forwards";
+    }
+});
+
+document.querySelector("#form_email").addEventListener("input", () => {
+    if(emailInput.checkValidity()) {
+        labelError.style.visibility = "hidden";
+        labelError.style.opacity = "0";
+    } else {
+        labelError.style.visibility = "visible";
+        labelError.style.opacity = "1";
+    }
+});
+
+document.querySelector("#success_btn").addEventListener("click", () => {
+    emailInput.value = "";
+    subscribeSection.style.animation = "cardRotationShow 1s ease-in-out forwards";
+    successSection.style.animation = "cardRotationHide 1s ease-in-out forwards";
 });
